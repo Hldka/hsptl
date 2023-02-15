87dc290d9784e3e1f4d9f62f5feff9e06464da00
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-15 11:57:37 +0100

    v2

--- src/main/java/com/lcb/exception/ConflictException.java
+++ src/main/java/com/lcb/exception/ConflictException.java
@@ -1,7 +1,6 @@
 package com.lcb.exception;
 
 public class ConflictException extends RuntimeException{
-    public ConflictException(String message ) {
-        super(message);
-    }
+    public ConflictException(String message){
+        super(message);}
 }
--- src/main/java/com/lcb/exception/ResourceNotFoundException.java
+++ src/main/java/com/lcb/exception/ResourceNotFoundException.java
@@ -1,7 +1,7 @@
 package com.lcb.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
+    public ResourceNotFoundException(String message){
+
+        super(message);}
 }
