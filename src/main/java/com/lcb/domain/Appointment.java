8fb322556263e07089993757b1164f3ba35facc3
yavuzce <celikselim14@gmail.com>
2023-02-15 15:40:43 +0100

    appointment olusturuldu!

--- src/main/java/com/lcb/domain/Appointment.java
+++ src/main/java/com/lcb/domain/Appointment.java
@@ -1,4 +1,44 @@
 package com.lcb.domain;
 
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
 public class Appointment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Setter(AccessLevel.NONE)
+    private Integer id;
+
+
+    @NotNull(message = "doctor id can not be null")
+    @NotBlank(message = "first character can not be with white space ")
+    @Column(unique = true)
+    private String doctorId;
+
+
+    @NotNull(message = "patient id can not be null")
+    @NotBlank(message = "first character can not be with white space ")
+    @Column(unique = true)
+    private String patientId;
+
+    private String data;
+
+    private  Integer hour;
+
+
+    private Integer minute;
+
+    private String notes;
+
+
+
+    
 }
