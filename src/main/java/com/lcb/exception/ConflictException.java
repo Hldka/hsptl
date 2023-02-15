ffcbeac40d41c408456b72fe7ccd687fbc3c35c3
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-15 11:57:37 +0100

    v2

---
+++ src/main/java/com/lcb/exception/ConflictException.java
@@ -0,0 +1,6 @@
+package com.lcb.exception;
+
+public class ConflictException extends RuntimeException{
+    public ConflictException(String message){
+        super(message);}
+}
---
+++ src/main/java/com/lcb/service/ServiceClass.java
@@ -0,0 +1,7 @@
+package com.lcb.service;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+public class ServiceClass {
+}
