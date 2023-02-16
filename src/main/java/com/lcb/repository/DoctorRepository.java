9369134b55feceee946424bc4465aec169e0c383
Fatih Hayyar <fthhyyr07@gmail.com>
2023-02-16 22:09:28 +0100

    fatih updated

---
+++ src/main/java/com/lcb/repository/DoctorRepository.java
@@ -0,0 +1,10 @@
+package com.lcb.repository;
+
+import com.lcb.domain.Doctor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DoctorRepository extends JpaRepository<Doctor,Long> {
+
+}
