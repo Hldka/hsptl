2cb1a794f00968c29b0674c6beff7bb87f28e00f
Hldka <116027001+Hldka@users.noreply.github.com>
2023-02-16 22:12:44 +0100

    v2

--- src/main/java/com/lcb/exception/ConflictException.java
+++ src/main/java/com/lcb/exception/ConflictException.java
@@ -1,6 +1,7 @@
 package com.lcb.exception;
 
 public class ConflictException extends RuntimeException{
-    public ConflictException(String message){
-        super(message);}
+    public ConflictException(String message ) {
+        super(message);
+    }
 }
