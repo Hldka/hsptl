ae30df6a9f3177bf3c85825d75bad54c6a036fe5
eozkanch <chofficelch@gmail.com>
2023-02-17 18:12:57 +0100

    git pull

--- src/main/java/com/lcb/controller/DoctorController.java
+++ src/main/java/com/lcb/controller/DoctorController.java
@@ -1,69 +1,24 @@
 package com.lcb.controller;
 
-
-
-import com.lcb.domain.Doctor;
-import com.lcb.dto.DoctorDTO;
-import com.lcb.exception.ConflictException;
 import com.lcb.service.DoctorService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import javax.validation.Valid;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/doctors")
-public class DoctorController {//http://localhost:8080/doctors
-    @Autowired
-    private DoctorService doctorService;
+public class DoctorController {
+    final DoctorService doctorService;
 
-    // dr ekle
-    @PostMapping
-    public ResponseEntity<String> saveDoctor(@Valid @RequestBody Doctor doctor){
-        doctorService.saveDoctor(doctor);
-        return ResponseEntity.ok("creating successfull");
-    }
-//bütün dr getir
-
-
-
-    @GetMapping("/List")
-    public ResponseEntity<List<Doctor>> getAllList(){
-        List<Doctor> list=doctorService.getAllList();
-        return ResponseEntity.ok(list);
-    }
-    //Id ile görüntüleme
-    @GetMapping("/{id}")
-    public ResponseEntity<Doctor> getDoctorById(@PathVariable("id") Long id){
-        Doctor doctor=doctorService.findDoctor(id);
-        return ResponseEntity.ok(doctor);
+    public DoctorController(DoctorService doctorService) {
+        this.doctorService = doctorService;
     }
 
-
-    //dr sil
     @DeleteMapping("/{id}")
     public ResponseEntity<String> deleteDoctor(@PathVariable("id") Long id){
         doctorService.deleteDoctor(id);
         return ResponseEntity.ok("Doctor deleted successfully");
     }
-
-    //dr güncelle
-    @PutMapping("{id}")
-    public ResponseEntity<Map<String,String>> updateDoctor(@PathVariable("id")Long id,
-                                                           @Valid @RequestBody DoctorDTO doctorDTO){
-        doctorService.updateDoctor(id,doctorDTO);
-        Map<String,String> map = new HashMap<>();
-        map.put("message","Doctor is updated successfuly");
-        map.put("status" ,"true");
-
-        return new ResponseEntity<>(map, HttpStatus.OK);
-    }
-
-
 }
--- src/main/java/com/lcb/service/DoctorService.java
+++ src/main/java/com/lcb/service/DoctorService.java
@@ -1,60 +1,23 @@
 package com.lcb.service;
 
 import com.lcb.domain.Doctor;
-
-
-import com.lcb.dto.DoctorDTO;
-import com.lcb.exception.ConflictException;
 import com.lcb.exception.ResourceNotFoundException;
 import com.lcb.repository.DoctorRepository;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.List;
 
 @Service
 public class DoctorService {
+    final DoctorRepository doctorRepository;
 
-    @Autowired
-    private DoctorRepository doctorRepository;
+    public DoctorService(DoctorRepository doctorRepository) {
+        this.doctorRepository = doctorRepository;
+    }
+
     public void deleteDoctor(Long id) {
         Doctor doctor=doctorRepository.findById(id).
                 orElseThrow(()->new ResourceNotFoundException("Doctor not found"));
         doctorRepository.delete(doctor);
 
 
-    }
-
-
-    public void saveDoctor(Doctor doctor) {
-        if (doctorRepository.existsByPersonalNo(doctor.getPersonalNo())){
-            throw new ConflictException("Doctor already exist");
-
-        }
-        doctorRepository.save(doctor);
-    }
-
-
-    public List<Doctor> getAllList() {
-        return doctorRepository.findAll();
-    }
-
-    public Doctor findDoctor(Long id) {
-        return doctorRepository.findById(id).orElseThrow(()->
-                new ResourceNotFoundException("Doctor is not found"+id));
-    }
-
-    public void updateDoctor(Long id, DoctorDTO doctorDTO) {
-        boolean existPersonalNo=doctorRepository.existsByPersonalNo(doctorDTO.getPersonalNo());
-        Doctor doctor=findDoctor(id);
-        if (existPersonalNo&&!doctorDTO.getPersonalNo().equals(doctor.getPersonalNo())){
-            throw new ConflictException("PersonalNo is already exist");
-        }
-        doctor.setName(doctorDTO.getName());
-        doctor.setNamePrefix(doctorDTO.getNamePrefix());
-        doctor.setDepartment(doctorDTO.getDepartment());
-        doctor.setDateOfGraduate(doctorDTO.getDateOfGraduate());
-        doctor.setDateOfStart(doctorDTO.getDateOfStart());
-        doctorRepository.save(doctor);
     }
 }
